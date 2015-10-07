5a46dd7dc98fa998f7bed97cdea3c5490e7f74dd
Charlie Pugh <cpugh@tccm.org>
2015-10-07 12:22:37 -0400

    Moved search value into the state of the App Component.

--- public/js/components/App.jsx
+++ public/js/components/App.jsx
@@ -7,26 +7,24 @@ var App = React.createClass({
 			image: "",
 			limit: 21,
 			offset: 0,
-			search: false
+			search: false,
+			searchValue: ''
 		}
 	},
-	onChange: function(event) {
-	
+	onSearchValueChange: function (searchValue) {
 		this.setState({
-			offset: 0
+			offset: 0,
+			searchValue: searchValue
 		});
-		
-		// get the input value
-		var artist = $('#artist').val();
-		if (artist != "") {
+
+		if (this.state.searchValue != '') {
 			// build the url string
-			var albums_url = "https://api.spotify.com/v1/search?limit=21&offset=0&type=album,artist&q=artist:" + encodeURIComponent(artist);
+			var albums_url = "https://api.spotify.com/v1/search?limit=21&offset=0&type=album,artist&q=artist:" + encodeURIComponent(this.state.searchValue);
 			// make the api call
-			
 			var self = this;
-			
+
 			$.ajax(albums_url).done(function(res){
-		
+
 				self.setState({
 					albums: res.albums.items,
 					artist: res.artists.items[0],
@@ -34,11 +32,11 @@ var App = React.createClass({
 					image: res.artists.items[0].images[0].url,
 					search: 1
 				});
-			
+
 			});
-			
+
 		} else {
-		
+
 			this.setState({
 				albums: [],
 				artist: [],
@@ -49,64 +47,63 @@ var App = React.createClass({
 		}
 	},
 	onLoadMore: function() {
-		
+
 		var newOffset = this.state.offset + 21;
-		
-		var artist = $('#artist').val();
 
 		// build the url string
-		var albums_url = "https://api.spotify.com/v1/search?limit=" + this.state.limit + "&offset=" + newOffset + "&type=album,artist&q=artist:" + encodeURIComponent(artist);
+		var albums_url = "https://api.spotify.com/v1/search?limit=" + this.state.limit + "&offset=" + newOffset + "&type=album,artist&q=artist:" + encodeURIComponent(this.state.searchValue);
 		// make the api call
-			
+
 		var albumsArray = this.state.albums;
 		var self = this;
-		
+
 		$.ajax(albums_url).done(function(res){
-			
+
 			var newAlbumArray = albumsArray.concat(res.albums.items);
-	
+
 			self.setState({
 				albums: newAlbumArray,
 				offset: newOffset
 			});
-		
+
 		});
-		
+
 	},
 	render: function() {
-		
+
 		return (
-		
+
 			<div>
-				
+
 				<Background image={this.state.image}/>
-				
+
 				<div id="container"><div id="contentWrap">
-			
+
 					<h1>React Tunes</h1>
-					
-					<Search onChange={this.onChange}/>
-					
+
+					<Search value={this.state.searchValue}
+									onChange={this.onSearchValueChange} />
+
 					{this.state.search && (
-						
+
 						<div>
-					
+
 							<ResultCount count={this.state.count}/>
-							
+
 							<ResultArtist artist={this.state.artist}/>
-							
+
 							<ResultAlbums ref="albums" albums={this.state.albums}/>
-							
+
 							<LoadMore onLoadMore={this.onLoadMore}/>
-							
+
 						</div>
-					
+
 					)}
-				
+
 				</div></div>
-				
+
 			</div>
-		
+
 		);
 	}
 });
