1b91ea99243a61393aef9832db413d1793974d56
Charlie Pugh <cpugh@tccm.org>
2015-10-07 16:20:52 -0400

    Abstracted to Spotify component and moved API call into separate function.

--- public/js/components/Search.jsx
+++ public/js/components/Search.jsx
@@ -3,7 +3,6 @@ var Search = React.createClass({
 		this.props.onChange(event.target.value);
 	},
 	render: function () {
-		console.log(this.props);
 		return (
 			<div>
 				<input onChange={this.handleChange} value={this.props.value} ref="search" type="text" id="artist" placeholder="Search an Artist"/>
